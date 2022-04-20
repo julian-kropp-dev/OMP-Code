a7928dc2a8afbb2cd8732cf38da0064654ee5827
julian-kropp-dev-MacBook <77173321+julian-kropp-dev@users.noreply.github.com>
2022-04-20 21:02:56 +0200

    created file

--- src/Copy.java
+++ src/Copy.java
@@ -1,20 +1,18 @@
 //Frage: Gibt es eine interne Funktion für einen Deep Copy bei Klassenobjekten in Java?
 
-import javax.swing.*;
-
 class Objekt <T> {
-    private T i;
+    private T attribut;
 
     public Objekt(T i) {
-        this.i = i;
+        this.attribut = i;
     }
 
-    public T getI() {
-        return i;
+    public T getAttribut() {
+        return attribut;
     }
 
-    public void setI(T i) {
-        this.i = i;
+    public void setAttribut(T attribut) {
+        this.attribut = attribut;
     }
 }
 
@@ -26,21 +24,21 @@ class Main {
         //Kopie erstellen
         Objekt<Integer> shallowCopy = original;
 
-        shallowCopy.setI(2);
+        shallowCopy.setAttribut(2);
 
         System.out.println("----------Shallow Copy------------");
-        System.out.println("Original: " + original.getI()); //a wird nicht verändert
-        System.out.println("Shallow Copy: " + shallowCopy.getI()); //b wird auf 2 verändert
+        System.out.println("Original: " + original.getAttribut()); //a wird nicht verändert
+        System.out.println("Shallow Copy: " + shallowCopy.getAttribut()); //b wird auf 2 verändert
 
         //Klassen sind Referenztypen; dh. es handelt sich um ein shallow copy: Gewünscht: Deep Copy
-        Objekt<Integer> deepCopy = new Objekt<>(original.getI());
+        Objekt<Integer> deepCopy = new Objekt<>(original.getAttribut());
 
-        deepCopy.setI(3);
+        deepCopy.setAttribut(3);
 
 
         System.out.println("----------Deep Copy------------");
-        System.out.println("Original: " + original.getI());
-        System.out.println("Deep Copy: " + deepCopy.getI());
+        System.out.println("Original: " + original.getAttribut());
+        System.out.println("Deep Copy: " + deepCopy.getAttribut());
 
         //unnötig kompliziert, wenn Objekt viele Attribute hat. Swift nutzt dafür structs (value type)
 
