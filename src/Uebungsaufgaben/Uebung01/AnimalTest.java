1e1bf974fa49af52a7da7c2e514859d3285c802a
julian-kropp-dev-MacBook <77173321+julian-kropp-dev@users.noreply.github.com>
2022-04-24 09:56:31 +0200

    updated files

--- src/Uebungsaufgaben/Uebung01/AnimalTest.java
+++ src/Uebungsaufgaben/Uebung01/AnimalTest.java
@@ -12,7 +12,13 @@ class AnimalTest {
     Plant plant1 = new Plant("Gras", " ist grün");
     @Test
     void addFood() {
-        animal1.addFood(plant1);
-        System.out.println(Arrays.toString(animal1.getFoodPlants()));
+
+        //animal1.addFood(plant1);
+        assertTrue(animal1.onlyPlant());
+        assertTrue(animal1.onlyAnimal());
+    }
+    @Test
+    void getName() {
+        assertEquals("Zerba", animal1.getName());
     }
 }
