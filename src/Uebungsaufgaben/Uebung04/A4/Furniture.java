bfa6ff357385c4cc1a7ce3954bb8174d11f0e663
julian-kropp-dev-MacBook <77173321+julian-kropp-dev@users.noreply.github.com>
2022-05-18 13:19:14 +0200

    updated files

--- src/Uebungsaufgaben/Uebung04/A4/Furniture.java
+++ src/Uebungsaufgaben/Uebung04/A4/Furniture.java
@@ -1,5 +1,7 @@
 package Uebungsaufgaben.Uebung04.A4;
 
+import java.awt.*;
+
 public interface Furniture {
 }
 
@@ -16,28 +18,40 @@ class Desk extends Table {
 }
 
 class Room {
-    protected Furniture furniture[];
+    protected Furniture[] furniture;
 
     public Room(Furniture[] furniture) {
         this.furniture = furniture;
     }
 }
 
-abstract class Office extends Room {
-    private Chair chars[];
+class Office extends Room {
+    private Chair chairs[];
     private Desk desks[];
 
-    public Office(Furniture[] furniture, Chair[] chars, Desk[] desks) {
+    public Office(Furniture[] furniture, Chair[] chairs, Desk[] desks) {
         super(furniture);
-        if (chars.length == 0) {
+        if (chairs.length == 0) {
             throw new IllegalArgumentException("u have to have at least one chair in ur office, how else do u want to work boy?");
         }
-        this.chars = chars;
+        this.chairs = chairs;
 
         if (desks.length == 0) {
             throw new IllegalArgumentException("Are u crazy to work with ur laptop just on a chair? Keep aware of back pain!");
         }
         this.desks = desks;
     }
+}
+
+class Test {
+    Chair chair0 = new Chair();
+    Desk desk = new Desk();
+    Chair chair1 = new Chair();
+
+    Chair[] chairs = new Chair[]{chair0};
+    Furniture[] furnitures = new Furniture[]{chair0, desk, chair1};
+    Desk[] desks = new Desk[]{desk};
+
+    Office office = new Office(furnitures, chairs, desks);
 }
 
