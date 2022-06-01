17bd97930703d1e6baef991bc639bda5a9355917
Frauke Ahting <97278992+FAhting@users.noreply.github.com>
2022-06-01 17:52:04 +0200

    Aufgabe 3 bearbeitet

--- src/Uebungsaufgaben/Uebung06/A3/UnknownIdException.java
+++ src/Uebungsaufgaben/Uebung06/A3/UnknownIdException.java
@@ -1,4 +1,17 @@
 package Uebungsaufgaben.Uebung06.A3;
 
-public class UnknownIdException extends Throwable {
+public class UnknownIdException extends InvalidIdException {
+	
+	public UnknownIdException() {		
+	}
+	
+	public UnknownIdException(String inMessage) {
+		super(inMessage);
+	}
+
+	public UnknownIdException(String inMessage, int inErrorID ) {
+		super(inMessage, inErrorID);
+	}	
+
+
 }
