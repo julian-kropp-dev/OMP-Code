1b8f8820b048613a2c9569c7dab9cde3810d5765
Frauke Ahting <97278992+FAhting@users.noreply.github.com>
2022-06-01 17:47:34 +0200

    Aufgabe 3 bearbeitet

--- src/Uebungsaufgaben/Uebung06/A3/InvalidIdException.java
+++ src/Uebungsaufgaben/Uebung06/A3/InvalidIdException.java
@@ -1,6 +1,23 @@
 package Uebungsaufgaben.Uebung06.A3;
 
-public class InvalidIdException extends Throwable  {
-    public String getId() {
-    }
+public class InvalidIdException extends Exception {
+	
+	private int errorID;
+	
+	public InvalidIdException() {		
+	}
+	
+	public InvalidIdException(String inMessage) {
+		super(inMessage);
+	}
+	
+	public InvalidIdException(String inMessage, int inErrorID ) {
+		super(inMessage);
+		errorID = inErrorID;
+	}	
+		
+	public int getId() {
+		return errorID;
+	}
+	
 }
