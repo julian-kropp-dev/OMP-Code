32c6bd72e3df2b04c166054bb16cbd523ac1ea11
Frauke Ahting <97278992+FAhting@users.noreply.github.com>
2022-06-22 13:40:00 +0200

    Alternative Implementierung von Aufgabe 1

---
+++ src/Uebungsaufgaben/Uebung09/A1/Search.java
@@ -0,0 +1,56 @@
+package uebung09.a1;
+
+
+public class Search {
+	
+	private static int[] zahlen = new int[] {2, 7, 3, 9, 23};
+	private static boolean found;
+	
+	private static class SearchThread extends Thread{
+		
+		private int suchzahl;
+		private int startidx;
+		private int endidx;
+	
+		public SearchThread( int inSuchzahl, int inStartidx, int inEndidx ) {
+			suchzahl = inSuchzahl;
+			startidx = inStartidx;
+			endidx = inEndidx;
+		}
+		
+		@Override
+		public void run() {
+			// sucht Suchzahl in Array zahlen
+			for ( int i = startidx; i <= endidx; i++) {
+				if ( zahlen[i] == suchzahl ) {
+					System.out.println( currentThread().getName() + ": found" );
+					found = true;
+					return;
+				}
+			}
+			System.out.println( currentThread().getName() + ": not found" );
+			
+		}
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Automatisch generierter Methodenstub
+		int suchzahl = 7;
+		//
+		SearchThread left = new SearchThread(suchzahl, 0, 2);
+		SearchThread right = new SearchThread(suchzahl, 3, 4);
+		found = false;
+		left.start();
+		right.start();
+		try {
+			left.join();
+			right.join();
+		} catch ( InterruptedException e ) {}
+		System.out.println("Found: " + found);
+//		System.out.println("Done.");
+	}
+	
+
+
+}
